f80d09909193867e4b336235f44a2e759ad449b5
mrudulkolambe <mrudulkolambe02@gmail.com>
2024-04-11 18:03:36 +0530

    changes

--- src/components/ProductComponents/ProductRow.jsx
+++ src/components/ProductComponents/ProductRow.jsx
@@ -47,7 +47,7 @@ const ProductRow = ({ data }) => {
                 </td>
                 <td className="px-6 py-4">
                     <div className="flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => SetShowModal({ show: true, update: true, data: data })} className="hover:text-red-700" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-edit"><path d="M4 13.5V4a2 2 0 0 1 2-2h8.5L20 7.5V20a2 2 0 0 1-2 2h-5.5" /><polyline points="14 2 14 8 20 8" /><path d="M10.42 12.61a2.1 2.1 0 1 1 2.97 2.97L7.95 21 4 22l.99-3.95 5.43-5.44Z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => SetShowModal({ show: true, update: true, data: data })} className="hover:text-red-700 lucide lucide-file-edit" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 13.5V4a2 2 0 0 1 2-2h8.5L20 7.5V20a2 2 0 0 1-2 2h-5.5" /><polyline points="14 2 14 8 20 8" /><path d="M10.42 12.61a2.1 2.1 0 1 1 2.97 2.97L7.95 21 4 22l.99-3.95 5.43-5.44Z" /></svg>
                         <svg xmlns="http://www.w3.org/2000/svg" onClick={deleteProduct} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash"><path d="M3 6h18" /><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" /><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" /></svg>
                     </div>
                 </td>
